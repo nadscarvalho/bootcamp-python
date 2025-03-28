06f0a447a68022a0bc03ca0374c742e2ec2dbe85
nadinecarvalho <nadsmcarvalho@gmail.com>
2025-03-28 14:47:46 -0300

    alterações necessárias

--- modulo-3/1.1-listas/1.12-extend.py
+++ modulo-3/1.1-listas/1.12-extend.py
@@ -2,6 +2,9 @@ linguagens = ["python", "js", "c"]
 
 print(linguagens)
 
+linguagens2 = ["php", "ruby", "go"]
+linguagens.extend(linguagens2)
+print(linguagens)
 linguagens.extend(["java", "csharp"])
 
 print(linguagens)
